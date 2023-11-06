fc77843556c864d4c38646df6a4211f51995890d
Zirui Project <88216794+ziruiproject@users.noreply.github.com>
2023-11-06 18:40:26 +0700

    init hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,18 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import heroImage from './assets/hero.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div style={{ backgroundImage: `url(${heroImage})` }}
+        className='h-screen bg-cover bg-center bg-no-repeat'
+      >
+        <h1 className='font-serif font-semibold text-center text-4xl text-white'>Transforming Your Digital Dreams Into Reality</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
