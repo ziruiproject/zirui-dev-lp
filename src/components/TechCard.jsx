93ebbecef7ddea519b41df2304f61acc20803a7d
Zirui Project <88216794+ziruiproject@users.noreply.github.com>
2023-11-10 21:17:14 +0700

    making tech card

---
+++ src/components/TechCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function TechCard({ svgContent, index }) {
+    return (
+        <div className='flex flex-col items-center justify-center w-full'>
+            <div className={`glass rounded-15 flex flex-col items-center justify-center w-3/4 py-14 h-60 ${index === 1 ? 'my-10' : ''}`}>
+                {svgContent}
+            </div>
+        </div>
+    );
+}
+
+export default TechCard;
