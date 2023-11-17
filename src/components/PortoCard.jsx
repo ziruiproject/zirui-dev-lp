89abc5ee6658c80e652d0863d9297d491e7d747f
Zirui Project <88216794+ziruiproject@users.noreply.github.com>
2023-11-17 22:56:18 +0700

    updated portos

--- src/components/PortoCard.jsx
+++ src/components/PortoCard.jsx
@@ -6,9 +6,9 @@ function PortoCard() {
             <div className='flex items-center justify-between px-6 align-middle'>
                 <span className='pr-2 text-lg'>ZIRUI DEV</span>
                 <div href="/" className='flex items-center'>
-                    <span className='pr-2 text-lg'>TAKE A LOOK</span>
+                    <span className='pr-3 text-lg'>TAKE A LOOK</span>
                     <svg xmlns="http://www.w3.org/2000/svg" width="29" height="29" viewBox="0 0 29 29" fill="none">
-                        <path d="M11.125 4.375H4.375C3.47989 4.375 2.62145 4.73058 1.98851 5.36351C1.35558 5.99645 1 6.85489 1 7.75V24.625C1 25.5201 1.35558 26.3785 1.98851 27.0115C2.62145 27.6444 3.47989 28 4.375 28H21.25C22.1451 28 23.0035 27.6444 23.6365 27.0115C24.2694 26.3785 24.625 25.5201 24.625 24.625V17.875M17.875 1H28M28 1V11.125M28 1L11.125 17.875" stroke="white" stroke-linecap="round" stroke-linejoin="round" />
+                        <path d="M11.125 4.375H4.375C3.47989 4.375 2.62145 4.73058 1.98851 5.36351C1.35558 5.99645 1 6.85489 1 7.75V24.625C1 25.5201 1.35558 26.3785 1.98851 27.0115C2.62145 27.6444 3.47989 28 4.375 28H21.25C22.1451 28 23.0035 27.6444 23.6365 27.0115C24.2694 26.3785 24.625 25.5201 24.625 24.625V17.875M17.875 1H28M28 1V11.125M28 1L11.125 17.875" stroke="#181818" strokeWidth={0.4} stroke-linecap="round" stroke-linejoin="round" />
                     </svg>
                 </div>
             </div>
