4d4c6849b68ec871708ec51fcd373b1db08e1af9
Zirui Project <88216794+ziruiproject@users.noreply.github.com>
2023-11-17 19:41:20 +0700

    switch flex to grid

--- src/components/TechCard.jsx
+++ src/components/TechCard.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 function TechCard({ svgContent, index }) {
     return (
         <div className='flex flex-col items-center justify-center w-full'>
-            <div className={`glass rounded-15 flex flex-col items-center justify-center w-3/4 py-14 h-60 ${index === 1 ? 'my-10' : ''}`}>
+            <div className={'glass rounded-15 flex flex-col items-center justify-center w-3/4 py-14 h-60'}>
                 {svgContent}
             </div>
-        </div>
+        </div >
     );
 }
 
