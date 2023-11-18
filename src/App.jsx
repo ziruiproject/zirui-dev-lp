3ee290b56a938ab49f9121c0bc0c92d383438ea9
Zirui Project <88216794+ziruiproject@users.noreply.github.com>
2023-11-18 23:35:11 +0700

    added contact pages

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,11 @@ import PortoCard from './components/PortoCard'
 
 import './App.css'
 
-
 function App() {
 
+  const coloredStyle = {
+    color: '#08FFC8',
+  }
 
   return (
     <>
@@ -18,7 +20,7 @@ function App() {
       <Technologies></Technologies>
       <section className='px-6 pb-12'>
         <Title normal="Webfolio: Navigating Our" highlighted="Past Projects"></Title>
-        <div className='grid grid-cols-1 gap-y-7 '>
+        <div className='gap-y-7 grid grid-cols-1'>
           <PortoCard></PortoCard>
           <PortoCard></PortoCard>
           <PortoCard></PortoCard>
@@ -28,9 +30,18 @@ function App() {
       <section>
 
       </section>
-      <section className='flex flex-col items-center'>
-        <Title className='text-center' normal="Let's Work" highlighted=" With Us"></Title>
-        <img src={"./imgs/work-wus.png"} alt="" />
+      <section className='flex flex-col items-center w-screen h-screen px-6'>
+        <h2 className='pt-10 text-5xl font-semibold text-center'>
+          Let's Talk, <br />
+          <span style={coloredStyle}>No Cost, <br />
+          </span> Just Answers.
+        </h2>
+        <img src={"./imgs/lets-talk.png"} alt="" />
+        <form action="get">
+          <input placeholder='Enter your email here' className='form-card focus:outline-none px-4 py-2 scale-150' type="text" />
+        </form>
+        <span className='pt-5'> It's just between us, and we're here to help!</span>
+        <span>Or You can reach us via:</span>
       </section>
 
     </>
