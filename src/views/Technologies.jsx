bfbeca10e613b39e3fdcbe00e4b0bcef7e2109af
Zirui Project <88216794+ziruiproject@users.noreply.github.com>
2023-11-19 13:52:59 +0700

    building responsiveness

--- src/views/Technologies.jsx
+++ src/views/Technologies.jsx
@@ -4,10 +4,10 @@ import Title from '../components/Title';
 function Technologies() {
 
     return (
-        <div className='h-screen bg-green py-8 flex flex-col justify-center items-center px-6'>
-            <Title className='md:text-center' normal="Mastering Most of" highlighted="Modern Technologies" />
+        <div className='px-6'>
+            <Title className='' normal="Mastering Most of" highlighted="Modern Technologies" />
             {/* <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi expedita ducimus vero ex labore at doloremque. Consectetur sapiente laboriosam dolor placeat, dolore ex, quo vero dolorum, nostrum eius modi possimus!</p> */}
-            <img className='w-2/3' src={"./imgs/techs.png"} alt="" />
+            <img src={"./imgs/techs.png"} alt="" />
         </div>
     );
 }
