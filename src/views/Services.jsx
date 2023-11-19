97a8465e4dd292d78f7391c3e69f4390cc39eea1
Zirui Project <88216794+ziruiproject@users.noreply.github.com>
2023-11-19 16:15:11 +0700

    building responsiveness

--- src/views/Services.jsx
+++ src/views/Services.jsx
@@ -3,16 +3,16 @@ import Title from '../components/Title';
 
 function Services() {
     return (
-        <section className='md:h-screen px-10 bg-white md:flex md:flex-row-reverse md:justify-between'>
+        <section className='md:h-screen md:flex md:flex-row-reverse md:justify-between px-10 bg-white'>
             <div className='md:w-2/3 md:flex md:flex-col md:justify-center md:pl-12'>
                 <Title normal="We're Here to Serve" highlighted="What You Need" />
-                <p className='hidden md:block text-lg'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut voluptatem quibusdam delectus iusto cupiditate corrupti corporis aut obcaecati voluptatibus vel incidunt fugit, aspernatur tempore facilis soluta suscipit ipsam et excepturi.</p>
+                <p className='md:block hidden text-lg'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut voluptatem quibusdam delectus iusto cupiditate corrupti corporis aut obcaecati voluptatibus vel incidunt fugit, aspernatur tempore facilis soluta suscipit ipsam et excepturi.</p>
             </div>
-            <div className='container grid gap-5 place-items-center md:grid-cols-2 md:auto-rows-fr md:place-items-start md:w-5/6 md:my-8'>
-                <div className='h-full flex flex-col items-center justify-center w-3/4 p-4 text-center align-middle service-card md:w-full'>
-                    {/* <div className='fixed w-full h-full shadow-card left-5 top-5 -z-50'> */}
+            <div className='place-items-center md:grid-cols-2 md:auto-rows-fr md:place-items-start md:w-5/6 md:my-8 container grid gap-5'>
+                <div className='service-card flex flex-col items-center justify-center w-5/6 h-full p-4 text-center align-middle'>
+                    {/* <div className='shadow-card left-5 top-5 -z-50 fixed w-full h-full'> */}
                     {/* </div> */}
-                    <svg className='fixed -top-1/3 right-1/3' xmlns="http://www.w3.org/2000/svg" width="227" height="227" viewBox="0 0 227 227" fill="none">
+                    <svg className='-top-1/3 right-1/3 fixed' xmlns="http://www.w3.org/2000/svg" width="227" height="227" viewBox="0 0 227 227" fill="none">
                         <g filter="url(#filter0_f_225_5424)">
                             <circle cx="113.5" cy="113.5" r="30" fill="#39A1CC" />
                         </g>
@@ -29,10 +29,10 @@ function Services() {
                     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
                         Cupiditate at commodi officia, nobis, distinctio omnis, magnam.</p>
                 </div>
-                <div className='h-full flex flex-col items-center justify-center w-3/4 p-4 text-center align-middle service-card md:w-full'>
-                    {/* <div className='fixed w-full h-full shadow-card left-5 top-5 -z-50'> */}
+                <div className='service-card flex flex-col items-center justify-center w-5/6 h-full p-4 text-center align-middle'>
+                    {/* <div className='shadow-card left-5 top-5 -z-50 fixed w-full h-full'> */}
                     {/* </div> */}
-                    <svg className='fixed -top-1/3 right-1/3' xmlns="http://www.w3.org/2000/svg" width="227" height="227" viewBox="0 0 227 227" fill="none">
+                    <svg className='-top-1/3 right-1/3 fixed' xmlns="http://www.w3.org/2000/svg" width="227" height="227" viewBox="0 0 227 227" fill="none">
                         <g filter="url(#filter0_f_225_5424)">
                             <circle cx="113.5" cy="113.5" r="30" fill="#FF5163" />
                         </g>
@@ -49,10 +49,10 @@ function Services() {
                     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
                         Cupiditate at commodi officia, nobis, distinctio omnis, magnam.</p>
                 </div>
-                <div className='h-full flex flex-col items-center justify-center w-3/4 p-4 text-center align-middle service-card md:w-full'>
-                    {/* <div className='fixed w-full h-full shadow-card left-5 top-5 -z-50'> */}
+                <div className='service-card flex flex-col items-center justify-center w-5/6 h-full p-4 text-center align-middle'>
+                    {/* <div className='shadow-card left-5 top-5 -z-50 fixed w-full h-full'> */}
                     {/* </div> */}
-                    <svg className='fixed -top-1/3 right-1/3' xmlns="http://www.w3.org/2000/svg" width="227" height="227" viewBox="0 0 227 227" fill="none">
+                    <svg className='-top-1/3 right-1/3 fixed' xmlns="http://www.w3.org/2000/svg" width="227" height="227" viewBox="0 0 227 227" fill="none">
                         <g filter="url(#filter0_f_225_5424)">
                             <circle cx="113.5" cy="113.5" r="30" fill="#08FFC8" />
                         </g>
@@ -69,9 +69,9 @@ function Services() {
                     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
                         Cupiditate at commodi officia, nobis, distinctio omnis, magnam.</p>
                 </div>
-                <div className='h-full flex flex-col items-center justify-center w-3/4 p-4 text-center align-middle service-card md:w-full'>
-                    {/* <div className='fixed w-full h-full shadow-card left-5 top-5 -z-50'></div> */}
-                    <svg className='fixed -top-1/3 right-1/3' xmlns="http://www.w3.org/2000/svg" width="227" height="227" viewBox="0 0 227 227" fill="none">
+                <div className='service-card flex flex-col items-center justify-center w-5/6 h-full p-4 text-center align-middle'>
+                    {/* <div className='shadow-card left-5 top-5 -z-50 fixed w-full h-full'></div> */}
+                    <svg className='-top-1/3 right-1/3 fixed' xmlns="http://www.w3.org/2000/svg" width="227" height="227" viewBox="0 0 227 227" fill="none">
                         <g filter="url(#filter0_f_225_5424)">
                             <circle cx="113.5" cy="113.5" r="38.5" fill="#ECC285" />
                         </g>
