b3c7b63f838c81208a683aa5f783385d492964af
AddienZakia <addienzakia1903@gmail.com>
2023-11-30 01:43:05 +0700

    change something

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import heroImage from './assets/hero.png'
-import './App.css'
+import heroImage from "./assets/hero.png";
+import "./App.css";
 
 function App() {
-
   return (
     <>
-      <div style={{ backgroundImage: `url(${heroImage})` }}
-        className='h-screen bg-cover bg-center bg-no-repeat'
+      <div
+        style={{ backgroundImage: `url(${heroImage})` }}
+        className="h-screen bg-cover bg-center bg-no-repeat"
       >
-        <h1 className='font-serif font-semibold text-center text-4xl text-white'>Transforming Your Digital Dreams Into Reality</h1>
+        <h1 className="font-serif font-semibold text-center text-4xl text-white">
+          Transforming Your Digital Dreams Into Reality
+        </h1>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
