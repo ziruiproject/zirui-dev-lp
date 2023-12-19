081724e4e68a71c5e2f9f0eb1b7dcb3c16f4271e
AddienZakia <addienzakia1903@gmail.com>
2023-12-20 00:48:27 +0700

    fix

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,4 @@
 module.exports = {
-<<<<<<< HEAD
     root: true,
     env: { browser: true, es2020: true },
     extends: [
@@ -20,24 +19,3 @@ module.exports = {
         "no-unused-vars": "off",
     },
 };
-=======
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
-}
->>>>>>> 68f68031c1530ebf4cc52d2903ba6c518d7a047e
