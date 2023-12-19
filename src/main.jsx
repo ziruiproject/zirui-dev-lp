f71397509eeb0ed7a60e49f05340755767522e15
AddienZakia <addienzakia1903@gmail.com>
2023-12-20 00:40:02 +0700

    prog

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
@@ -8,15 +7,3 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         <App />
     </React.StrictMode>,
 );
-=======
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
->>>>>>> 68f68031c1530ebf4cc52d2903ba6c518d7a047e
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite'
-<<<<<<< HEAD
-import react from '@vitejs/plugin-react-swc'
-=======
-import react from '@vitejs/plugin-react'
->>>>>>> 68f68031c1530ebf4cc52d2903ba6c518d7a047e
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+});
